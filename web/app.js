2eb308e61c76b1480a6bb5487656994a9be121d2
Schrodinger99 <salvarfu@gmail.com>
2025-04-24 09:50:17 -0600

    Cambios de formatos para subir a un S3 Bucket

--- web/app.js
+++ web/app.js
@@ -13,7 +13,7 @@ const ipAddress = process.env.C9_HOSTNAME ?? 'localhost';
 
 // Set the views directory path
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // Update static file serving
 app.use('/public', express.static(path.join(__dirname, 'public')));
